b029bceb0307a7445c978fe6879c5b6afddef41a
viistorrr <victormeza41@gmail.com>
2022-09-28 13:34:29 -0500

    google analytics added

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,9 +1,24 @@
 import { Head, Html, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html className="h-full bg-gray-50 antialiased" lang="en">
-      <Head />
+      <Head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-T9VG4H5QQ6"
+          strategy="afterInteractive"
+        ></Script>
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+       window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-YKNPYBQ0ZC');
+      `}
+        </Script>
+      </Head>
       <body className="flex h-full flex-col">
         <Main />
         <NextScript />
