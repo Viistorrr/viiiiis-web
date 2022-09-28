a71cd46c7e9bdb87c1bd5c46aeafab04e5a6460d
viistorrr <victormeza41@gmail.com>
2022-09-28 07:34:30 -0500

    update contact form

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -1,6 +1,4 @@
 import Head from 'next/head'
-import Link from 'next/link'
-
 import { AuthLayout } from '@components/AuthLayout'
 import { Button } from '@components/Button'
 import { SelectField, TextField } from '@components/Fields'
