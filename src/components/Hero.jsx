00f6e7ec15032ca024f32a9f3d1863a424c1b6d1
viistorrr <victormeza41@gmail.com>
2022-11-08 13:49:38 -0500

    updates hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,77 +1,101 @@
 import dynamic from 'next/dynamic'
-import Image from 'next/future/image'
-import clsx from 'clsx'
-import { Button } from '@components/Button'
-import { Container } from '@components/Container'
-import logoBbc from '@images/logos/bbc.svg'
-import logoCbs from '@images/logos/cbs.svg'
-import logoCnn from '@images/logos/cnn.svg'
-import logoFastCompany from '@images/logos/fast-company.svg'
-import logoForbes from '@images/logos/forbes.svg'
-import logoHuffpost from '@images/logos/huffpost.svg'
-import logoTechcrunch from '@images/logos/techcrunch.svg'
-import logoWired from '@images/logos/wired.svg'
 
 const DynamicChart = dynamic(() => import('@components/Chart'), { ssr: false })
 
 export function Hero() {
   return (
     <div className="overflow-hidden">
-      <Container>
-        <div className="lg:grid lg:grid-cols-12 lg:gap-x-8 lg:gap-y-20">
-          <div className="relative z-10 mx-auto max-w-2xl lg:col-span-7 lg:max-w-none lg:pt-6 xl:col-span-6">
-            <h1 className="text-4xl font-medium tracking-tight text-gray-900">
-              viiiiis en el 2023
-            </h1>
-            <p className="mt-6 text-lg text-gray-600">
-              Es simple, nuetra meta es ayudar directamente{' '}
-              <b>
-                <i>3.000 niños, 500 animales entre perros y gatos</i>
-              </b>{' '}
-              en situación de calle o vulnerabilidad en el 2023.
-            </p>
-            <div className="mt-4 flex flex-col">
-              <i>¿será posible?</i>
-              <br />
-              <i>Pues ya lo veremos! 😁✌🏾</i>
+      {/* Hero card */}
+      <div className="relative">
+        <div className="absolute inset-x-0 bottom-0 h-1/2 bg-gray-100" />
+        <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+          <div className="relative shadow-xl sm:overflow-hidden sm:rounded-2xl">
+            <div className="absolute inset-0">
+              <img
+                className="h-full w-full object-cover"
+                src="/assets/imgs/hero/kids.jpg"
+                alt="People working on laptops"
+              />
+              <div className="absolute inset-0 bg-yellow-200 mix-blend-multiply" />
             </div>
-            <div className="m-4 flex flex-wrap gap-4">
-              <Button href="/contact" variant="outline">
-                <span className="m-2">SOY EMPRESA</span>
-              </Button>
-              <Button href="/contact" variant="outline">
-                <span className="m-2">SOY FUNDACIÓN</span>
-              </Button>
+            <div className="relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8">
+              <h1 className="text-center text-4xl font-bold tracking-tight sm:text-5xl lg:text-6xl">
+                <span className="block text-3xl text-white">
+                  Lo que haces por otros, no es más que una semilla que siembras
+                  en tu propia vida
+                </span>
+              </h1>
+              <p className="mx-auto mt-6 max-w-lg text-center text-xl text-yellow-200 sm:max-w-3xl">
+                Ayudamos a conectar empresas que quieren contribuir y apoyar a
+                proyectos sociales y/o fundaciones de la ciudad de Medellín sin
+                que sea un proceso tedioso o poco confiable para ellas
+              </p>
+              <div className="mx-auto mt-10 max-w-sm sm:flex sm:max-w-none sm:justify-center">
+                <div className="space-y-4 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5 sm:space-y-0">
+                  <a
+                    href="#"
+                    className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-yellow-700 shadow-sm hover:bg-yellow-50 sm:px-8"
+                  >
+                    Ponte en contacto
+                  </a>
+                  <a
+                    href="#"
+                    className="flex items-center justify-center rounded-md border border-transparent bg-yellow-500 bg-opacity-60 px-4 py-3 text-base font-medium text-white shadow-sm hover:bg-opacity-70 sm:px-8"
+                  >
+                    Live demo
+                  </a>
+                </div>
+              </div>
             </div>
-          </div>
-          <div className="relative mt-12 sm:mt-20 lg:col-span-5 lg:row-span-2 lg:mt-0 xl:col-span-6">
-            <div className="w-full">
-              <DynamicChart />
-            </div>
-          </div>
-          <div className="relative -mt-4 lg:col-span-7 lg:mt-0 xl:col-span-6">
-            <ul
-              role="list"
-              className="mx-auto mt-8 flex max-w-xl flex-wrap justify-center gap-x-10 gap-y-8 lg:mx-0 lg:justify-start"
-            >
-              {[
-                ['Forbes', logoForbes],
-                ['TechCrunch', logoTechcrunch],
-                ['Wired', logoWired],
-                ['CNN', logoCnn, 'hidden xl:block'],
-                ['BBC', logoBbc],
-                ['CBS', logoCbs],
-                ['Fast Company', logoFastCompany],
-                ['HuffPost', logoHuffpost, 'hidden xl:block'],
-              ].map(([name, logo, className]) => (
-                <li key={name} className={clsx('flex', className)}>
-                  <Image src={logo} alt={name} className="h-8" unoptimized />
-                </li>
-              ))}
-            </ul>
           </div>
         </div>
-      </Container>
+      </div>
+
+      {/* Logo cloud */}
+      {/* <div className="bg-gray-100">
+        <div className="mx-auto max-w-7xl py-16 px-4 sm:px-6 lg:px-8">
+          <p className="text-center text-base font-semibold text-gray-500">
+            Trusted by over 5 very average small businesses
+          </p>
+          <div className="mt-6 grid grid-cols-2 gap-8 md:grid-cols-6 lg:grid-cols-5">
+            <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
+              <img
+                className="h-12"
+                src="https://tailwindui.com/img/logos/tuple-logo-gray-400.svg"
+                alt="Tuple"
+              />
+            </div>
+            <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
+              <img
+                className="h-12"
+                src="https://tailwindui.com/img/logos/mirage-logo-gray-400.svg"
+                alt="Mirage"
+              />
+            </div>
+            <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
+              <img
+                className="h-12"
+                src="https://tailwindui.com/img/logos/statickit-logo-gray-400.svg"
+                alt="StaticKit"
+              />
+            </div>
+            <div className="col-span-1 flex justify-center md:col-span-2 md:col-start-2 lg:col-span-1">
+              <img
+                className="h-12"
+                src="https://tailwindui.com/img/logos/transistor-logo-gray-400.svg"
+                alt="Transistor"
+              />
+            </div>
+            <div className="col-span-2 flex justify-center md:col-span-2 md:col-start-4 lg:col-span-1">
+              <img
+                className="h-12"
+                src="https://tailwindui.com/img/logos/workcation-logo-gray-400.svg"
+                alt="Workcation"
+              />
+            </div>
+          </div>
+        </div>
+      </div> */}
     </div>
   )
 }
