85f17f232ccad0f255740b63cfc75b73007fabb5
viistorrr <victormeza41@gmail.com>
2022-11-08 16:10:08 -0500

    update call to action txt

--- src/components/CallToAction.jsx
+++ src/components/CallToAction.jsx
@@ -12,17 +12,14 @@ export function CallToAction() {
       </div>
       <div className="relative mx-auto max-w-7xl px-4 py-12 sm:px-6 lg:px-8 lg:py-16">
         <div className="md:ml-auto md:w-1/2 md:pl-10">
-          <h2 className="text-lg font-semibold text-gray-300">
-            Award winning support
-          </h2>
+          <h2 className="text-lg font-semibold text-gray-300">viiiiisApp</h2>
           <p className="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">
-            We’re here to help
+            Gracias por querer sumarte
           </p>
           <p className="mt-3 text-lg text-gray-300">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et, egestas
-            tempus tellus etiam sed. Quam a scelerisque amet ullamcorper eu enim
-            et fermentum, augue. Aliquet amet volutpat quisque ut interdum
-            tincidunt duis.
+            Estamos construyendo una plataforma segura que nos permita conectar
+            personas y empresas como tú que quieren contribuir y apoyar a
+            proyectos sociales y fundaciones
           </p>
           <div className="mt-8">
             <div className="inline-flex rounded-md shadow">
@@ -30,7 +27,7 @@ export function CallToAction() {
                 href="#"
                 className="inline-flex items-center justify-center rounded-md border border-transparent bg-white px-5 py-3 text-base font-medium text-gray-900 hover:bg-gray-50"
               >
-                Visit the help center
+                Puedes registrarte aquí
                 <CircleBackground
                   className="-mr-1 ml-3 h-5 w-5 text-gray-400"
                   aria-hidden="true"
