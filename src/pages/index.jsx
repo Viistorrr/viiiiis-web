ce6b5d7ad5fc3ff3708b1358849e5708ecda048e
viistorrr <victormeza41@gmail.com>
2022-11-12 14:57:42 -0500

    add opengraph

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -12,7 +12,25 @@ export default function Home() {
         <title>viiiiis</title>
         <meta
           name="description"
-          content="viiiiis, fundation, donation, donaciones, fundaciones, non profit"
+          content="Lo que haces por otros, es una semilla que siembras en tu propia vida. Somos viiiis, una comunidad que sueña hacer visible lo pequeño y significativo lo que no creemos que es."
+        />
+        <meta
+          name="twitter:image"
+          content="https://www.viiiiis.com/assets/imgs/home/kids.jpg"
+        />
+        <meta
+          property="og:description"
+          content="Lo que haces por otros, es una semilla que siembras en tu propia vida. Somos viiiis, una comunidad que sueña hacer visible lo pequeño y significativo lo que no creemos que es."
+        />
+        <meta
+          property="og:image"
+          content="https://www.viiiiis.com/assets/imgs/home/kids.jpg"
+        />
+        <meta property="twitter:domain" content="www.viiiiis.com" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          name="twitter:description"
+          content="Lo que haces por otros, es una semilla que siembras en tu propia vida. Somos viiiis, una comunidad que sueña hacer visible lo pequeño y significativo lo que no creemos que es."
         />
       </Head>
       <main>
